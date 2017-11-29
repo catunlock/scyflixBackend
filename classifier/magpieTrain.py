299e7b7d2cce311f3d9f2dfa67d03d8ba77edc02
Alberto Lopez Sanchez <albertoxp1@gmail.com>
2017-11-29 20:01:12 +0100

    Update de el magpie para usar la ultima version de la API.

--- classifier/magpieTrain.py
+++ classifier/magpieTrain.py
@@ -1,5 +1,5 @@
 import os
-from magpie import MagpieModel
+from magpie import Magpie
 
 data = 'computer_science_magpie/'
 
@@ -12,20 +12,31 @@ def load_labels():
 
     return labels
 
-magpie = MagpieModel()
+def train_magpie(model):
+    print
+    "Training vectors..."
+    model.init_word_vectors(data, vec_dim=100)
 
-print "Training vectors..."
-magpie.init_word_vectors(data, vec_dim=100)
+    labels = load_labels()
+    print
+    labels
+
+    print
+    "Training labels..."
+    model.train(data, labels, test_ratio=0.2, nb_epochs=30)
+
+    print
+    "Saving..."
+    model.save_word2vec_model('./embeddings', overwrite=True)
+    model.save_scaler('./scaler', overwrite=True)
+    model.save_model('./model.h5', overwrite=True)
 
 
-labels = load_labels()
-print labels
-
-print "Training labels..."
-magpie.train(data, labels, test_ratio=0.2, nb_epochs=30)
-
-print "Saving..."
-magpie.save_word2vec_model('./embeddings' , overwrite=True)
-magpie.save_scaler('./scaler', overwrite=True)
-magpie.save_model('./model.h5')
+try:
+    magpie = Magpie(keras_model='model.h5', word2vec_model='embeddings', scaler='scaler')
+    train_magpie(magpie)
+except Exception:
+    print("Unable to load the model, training again.")
+    magpie = Magpie()
+    train_magpie(magpie)
 
