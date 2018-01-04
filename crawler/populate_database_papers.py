d7b6cf53c1d1a8a4a2c845ccbcbca605f5227bf2
Alberto Lopez <alberto.lopez.sanchez@est.fib.upc.edu>
2018-01-05 00:49:06 +0100

    Primera version de la populacion de la base de datos.

---
+++ crawler/populate_database_papers.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python3
+from pathlib import Path
+import xml.etree.ElementTree as ET
+import os
+
+from pymongo import MongoClient
+from datetime import datetime
+
+# Find by categori:
+# db.getCollection('papers').find({categories: {$eq: {name:'cs.CR'}}})
+
+# Finb by summary:
+# db.getCollection('papers').find({'summary': {$regex: ".*has.*"}})
+
+INDEX_PATH = "./index/"
+PAPERS_PATH = "./papers/"
+
+LABELS = set(["cs.AI", "cs.CR", "cs.CV", "cs.DB", "cs.LG"])
+
+def populate_index_file(path_file):
+    print("Populating:", str(path_file))
+    tree = ET.parse(str(path_file))
+    root = tree.getroot()
+
+    file_name = PAPERS_PATH + str(path_file.parents[0]) + "/"
+    if not os.path.exists(file_name):
+        os.makedirs(file_name)
+
+    entries = []
+
+    for entry in root.iter('{http://www.w3.org/2005/Atom}entry'):
+
+        properties = dict()
+        properties['categories'] = []
+
+        for c in entry:
+
+            if c.tag == '{http://www.w3.org/2005/Atom}id':
+                properties['id'] = c.text.split('/')[-1]
+            
+            if c.tag == '{http://www.w3.org/2005/Atom}link':
+                if 'type' in c.attrib and c.attrib['type'] == 'application/pdf':
+                    properties['link_pdf'] = c.attrib['href']
+
+            if c.tag == '{http://www.w3.org/2005/Atom}category':
+                label = c.attrib['term'].strip('\n')
+
+                if label in LABELS:
+                    properties['categories'].append({'name': label})
+
+            if c.tag == '{http://www.w3.org/2005/Atom}updated':
+                properties['updated'] = c.text
+
+            if c.tag == '{http://www.w3.org/2005/Atom}published':
+                properties['published'] = c.text
+
+            if c.tag == '{http://www.w3.org/2005/Atom}title':
+                properties['title'] = c.text
+
+            if c.tag == '{http://www.w3.org/2005/Atom}summary':
+                properties['summary'] = c.text
+
+            if c.tag == '{http://www.w3.org/2005/Atom}author':
+                properties['author'] = c.text
+
+            if c.tag == '{http://arxiv.org/schemas/atom}comment':
+                properties['comment'] = c.text
+
+        entries.append(properties)
+
+    print(entries)
+
+    client = MongoClient()
+    db = client.database
+
+    db.papers.insert(entries)
+    print("Get of bd:", db.papers.find_one())
+
+def travel_index(path_file):
+    print("Entering directory:" + str(path_file))
+    if path_file.is_dir():
+        for p in path_file.iterdir():
+            if p.is_dir():
+                travel_index(p)
+            else:
+                populate_index_file(p)
+    else:
+        populate_index_file(path_file)
+
+def main():
+    travel_index(Path(INDEX_PATH))
+
+if __name__ == "__main__":
+    main()
