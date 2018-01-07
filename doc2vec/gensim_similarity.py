cb19b73de9ddbba65cfaaad8b96d2d0a161a82bd
Alberto Lopez <alberto.lopez.sanchez@est.fib.upc.edu>
2018-01-07 19:43:24 +0100

    Ahora la API similarity busca por el id de arxiv.

--- doc2vec/gensim_similarity.py
+++ doc2vec/gensim_similarity.py
@@ -55,18 +55,20 @@ class GensimSimilarity:
     def getDocument(self, id):
         return self.corpus.getTaggedDocuments()[id]
 
-    def getDocumentName(self, id):
-        return self.corpus.getDocumentName(id)
+    def accessIndex(self, id):
+        return self.corpus.accessIndex(id)
 
     def generate_json_similars(self, doc_id):
         # {"patata" : [tal, cual, ...]}
         result = []
-        sims = self.getSimilarity(doc_id)[0:20]
+        id_paper = self.corpus.accessIndex(doc_id)
+        print("ID_PAPER:", id_paper)
+        sims = self.getSimilarity(id_paper)[0:20]
         for id, prob in sims:
             entry = {}
             entry["id"] = str(id)
-            entry["arxiv"] = str(self.getDocumentName(id))
-            entry["file_path"] = papers_path + str(self.getDocumentName(id))[:-4] + '.pdf'
+            entry["arxiv"] = str(self.accessIndex(id))
+            entry["file_path"] = papers_path + str(self.accessIndex(id))[:-4] + '.pdf'
             entry["document"] = self.getDocument((id))
             (a,b) = self.getDocument((id))
             #para mostrar los datos me va mejor no tener un TaggedDocument y que me vengan como lista de strings
@@ -82,8 +84,8 @@ class GensimSimilarity:
         for id, prob in sims:
             entry = {}
             entry["id"] = str(id)
-            entry["arxiv"] = str(self.getDocumentName(id))
-            entry["file_path"] = papers_path + str(self.getDocumentName(id))[:-4] + '.pdf'
+            entry["arxiv"] = str(self.accessIndex(id))
+            entry["file_path"] = papers_path + str(self.accessIndex(id))[:-4] + '.pdf'
             entry["document"] = self.getDocument((id))
             (a, b) = self.getDocument((id))
             # para mostrar los datos me va mejor no tener un TaggedDocument y que me vengan como lista de strings
@@ -106,7 +108,8 @@ class GensimCorpus:
             self.documents = list()
 
             for i, c in corpus:
-                self.index[i] = c[0]
+                self.index[i] = c[0][:-4]
+                self.index[c[0][:-4]] = i
                 doc = c[1]
                 self.documents.append(gensim.models.doc2vec.TaggedDocument(gensim.utils.simple_preprocess(doc), [i]))
 
@@ -114,13 +117,11 @@ class GensimCorpus:
     def getTaggedDocuments(self):
         return self.documents
 
-    def paper_from_id(self, id):
-        return index[id]
 
     def size(self):
         return len(self.documents)
 
-    def getDocumentName(self, id):
+    def accessIndex(self, id):
         return self.index[id]
 
 
@@ -142,6 +143,7 @@ if __name__ == "__main__":
     # Pick a random document from the test corpus and infer a vector from the model
     doc_id = random.randint(0, gs.size())
 
+    print("Test:", corpus_train.accessIndex("0504058v1"))
 
     sims = gs.getSimilarity(doc_id)
 
@@ -149,5 +151,5 @@ if __name__ == "__main__":
     print('Test Document ({}): «{}»\n'.format(doc_id, ' '.join(gs.getDocument(doc_id).words)))
     print(u'SIMILAR/DISSIMILAR DOCS PER MODEL %s:\n' % model)
     for index in range(20):
-        print(u'%s %s: «%s»\n' % (sims[index], gs.getDocumentName(sims[index][0]), ' '.join(gs.getDocument(sims[index][0]).words)))
+        print(u'%s %s: «%s»\n' % (sims[index], gs.accessIndex(sims[index][0]), ' '.join(gs.getDocument(sims[index][0]).words)))
 
--- doc2vec/scyflix_rest.py
+++ doc2vec/scyflix_rest.py
@@ -28,8 +28,8 @@ def hello_world():
 
 @app.route('/similarity')
 def similarity():
-    doc_id = int(request.args.get('doc_id', 10))
-    cosa = gs.generate_json_similars(int(doc_id))
+    doc_id = request.args.get('doc_id', '')
+    cosa = gs.generate_json_similars(doc_id)
     print(cosa)
     return app.response_class(
         response=json.dumps(cosa, separators=(',', " : "), indent=4),
